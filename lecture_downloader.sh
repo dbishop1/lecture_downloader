1cfaf3f600520e1bb2481e1dfb0335922f6b2dd7
Darren Bishop <D_R_Bish@hotmail.com>
2015-08-05 09:00:55 +1000

    added destination directory

--- lecture_downloader.sh
+++ lecture_downloader.sh
@@ -1,3 +1,6 @@
+#download directory
+dest=/home/darren/Documents/comp4610/lecture_notes/
+
 #download the index file
 wget --quiet --no-remove-listing http://cs.anu.edu.au/student/comp4610/notes/  
 
@@ -14,10 +17,11 @@ else
     #download the file if it doesn't already exist
     for i in ${arr[@]}; do
         url="http://cs.anu.edu.au/student/comp4610/notes/$i"
-        if [ ! -f $i ]; then
+        filedir="$dest$i"
+        if [ ! -f $filedir ]; then
             no_files_downloaded=0
             echo "Downloading $i"
-            wget --quiet $url
+            wget --quiet --directory-prefix=$dest $url
         fi
     done
     if [ $no_files_downloaded -eq 1 ]; then
